8f7c0d99f1579c7d0e32e6891db787e4de702c72
kentrung <trungnt256@gmail.com>
2026-05-24 10:24:16 +0700

    Fix base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'https://github.com/kentrung/son-linh-reactjs-select-teacher/',
+  base: '/son-linh-reactjs-select-teacher/',
 })
